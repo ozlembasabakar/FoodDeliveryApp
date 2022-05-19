7a38fed02043230297c8531485259a24b07f0e8c
Ozlem Basabakar <ozlembasabakar@gmail.com>
2022-05-19 14:47:28 +0300

    add parcelize, navController, bundle, detail layouts

--- app/src/main/java/com/example/fooddelivery/MainActivity.kt
+++ app/src/main/java/com/example/fooddelivery/MainActivity.kt
@@ -6,10 +6,13 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Divider
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -21,13 +24,27 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.fooddelivery.ui.theme.*
+
+object Destinations {
+    const val Home = "Home"
+    const val Detail = "Detail"
+
+    object DetailArgs {
+        const val foodData = "foodData"
+    }
+}
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,13 +52,30 @@ class MainActivity : ComponentActivity() {
         setContent {
             FoodDeliveryTheme {
 
+                val navController = rememberNavController()
+
+                NavHost(
+                    navController = navController,
+                    startDestination = Destinations.Home,
+                    builder = {
+
+                        composable(Destinations.Home) {
+                            HomeScreen(navController = navController)
+                        }
+
+                        composable(Destinations.Detail) {
+                            DetailScreen(navController = navController)
+                        }
+
+                    })
+
             }
         }
     }
 }
 
 @Composable
-fun HomeScreen() {
+fun HomeScreen(navController: NavController) {
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -105,7 +139,7 @@ fun HomeScreen() {
                     ),
                     PopularData(
                         R.drawable.primavera_pizza,
-                        title = "Primavera Salad",
+                        title = "Primavera Pizza",
                         description = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form.",
                         price = 12.55,
                         calorie = 450.0,
@@ -119,10 +153,281 @@ fun HomeScreen() {
                             R.drawable.ing5
                         )
                     )
-
-                )
+                ),
+                navController = navController
             )
         }
+    }
+}
+
+@Composable
+fun DetailScreen(navController: NavController) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+            .padding(start = 30.dp, top = 40.dp, end = 30.dp),
+        contentAlignment = Alignment.TopCenter
+    ) {
+        val data =
+            navController.previousBackStackEntry?.arguments?.getParcelable<PopularData>(Destinations.DetailArgs.foodData)
+
+        if (data != null) {
+            Text(text = data.title)
+        } else {
+            //TODO: show only design screen
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                DetailHeader()
+
+                Spacer(modifier = Modifier.height(32.dp))
+
+                Image(
+                    painter = painterResource(id = R.drawable.salad_pesto_pizza),
+                    contentDescription = "",
+                    modifier = Modifier.size(275.dp)
+                )
+
+                Spacer(modifier = Modifier.height(20.dp))
+
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(80.dp)
+                )
+                {
+                    Column(verticalArrangement = Arrangement.SpaceBetween) {
+                        Text(
+                            text = "Salad Pesto Pizza",
+                            color = BlackTextColor,
+                            style = Typography.body1,
+                            fontSize = 22.sp
+                        )
+
+                        Row(verticalAlignment = Alignment.CenterVertically) {
+                            Text(
+                                buildAnnotatedString {
+                                    withStyle(
+                                        style = SpanStyle(
+                                            color = Orange500,
+                                            fontStyle = Typography.body1.fontStyle,
+                                            fontSize = 20.sp
+                                        )
+                                    ) {
+                                        append(
+                                            "$"
+                                        )
+                                    }
+
+                                    withStyle(
+                                        style = SpanStyle(
+                                            color = BlackTextColor,
+                                            fontStyle = Typography.body1.fontStyle,
+                                            fontSize = 28.sp
+                                        )
+                                    ) {
+                                        append(
+                                            "10.55"
+                                        )
+                                    }
+                                }
+                            )
+                        }
+                    }
+
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        BoxWithRes(
+                            resId = R.drawable.minus,
+                            description = "Minus",
+                            boxSize = 36,
+                            iconSize = 24,
+                            iconColor = BlackTextColor
+                        )
+
+                        Spacer(modifier = Modifier.width(14.dp))
+
+                        Text(
+                            text = "01",
+                            style = Typography.body2,
+                            color = TextColor,
+                            fontSize = 18.sp,
+                        )
+
+                        Spacer(modifier = Modifier.width(14.dp))
+
+                        BoxWithRes(
+                            resId = R.drawable.add,
+                            description = "Add",
+                            boxSize = 36,
+                            iconSize = 24,
+                            iconColor = Color.White,
+                            bgColor = Yellow500
+                        )
+                    }
+
+                }
+
+                Spacer(modifier = Modifier.height(20.dp))
+
+                Text(
+                    text = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form.",
+                    style = Typography.h5,
+                    color = TextColor,
+                    fontSize = 16.sp,
+                    modifier = Modifier.fillMaxWidth()
+                )
+
+                Spacer(modifier = Modifier.height(20.dp))
+
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(72.dp)
+                        .clip(RoundedCornerShape(14.dp))
+                        .background(CardItemBg)
+                        .padding(15.dp)
+                ) {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.calori),
+                            contentDescription = "Calorie",
+                            modifier = Modifier.size(20.dp)
+                        )
+
+                        Divider(color = DividerColor,modifier = Modifier.fillMaxHeight().width(1.dp))
+
+                        Image(
+                            painter = painterResource(id = R.drawable.star),
+                            contentDescription = "Calorie",
+                            modifier = Modifier.size(20.dp)
+                        )
+
+                        Divider(color = DividerColor,modifier = Modifier.fillMaxHeight().width(1.dp))
+
+                        Image(
+                            painter = painterResource(id = R.drawable.schedule),
+                            contentDescription = "Calorie",
+                            modifier = Modifier.size(20.dp)
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Header() {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(end = 13.dp)
+    ) {
+        BoxWithRes(resId = R.drawable.menu, description = "Menu")
+
+        Row(verticalAlignment = Alignment.CenterVertically)
+        {
+            Icon(
+                painter = painterResource(id = R.drawable.location),
+                contentDescription = "Location",
+                modifier = Modifier.size(16.dp),
+                tint = Orange500
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(text = "California, US")
+            Spacer(modifier = Modifier.width(8.dp))
+            Icon(
+                painter = painterResource(id = R.drawable.arrow_down),
+                contentDescription = "Arrow down",
+                modifier = Modifier.size(16.dp),
+                tint = Orange500
+            )
+        }
+
+        BoxWithRes(resId = R.drawable.search, description = "Search")
+
+    }
+}
+
+@Composable
+fun DetailHeader() {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        BoxWithRes(resId = R.drawable.arrow_left, description = "Menu")
+
+        Box(
+            modifier = Modifier
+                .size(40.dp)
+                .clip(RoundedCornerShape(10.dp))
+                .background(CardItemBg),
+            contentAlignment = Alignment.Center
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(24.dp)
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.bag),
+                    contentDescription = "",
+                    modifier = Modifier.size(24.dp),
+                    tint = IconColor
+                )
+
+                Box(
+                    modifier = Modifier
+                        .padding(top = 5.dp, end = 5.dp)
+                        .size(8.dp)
+                        .clip(CircleShape)
+                        .background(Color.White)
+                        .align(Alignment.TopEnd),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .size(6.dp)
+                            .clip(CircleShape)
+                            .background(Yellow500)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun BoxWithRes(
+    resId: Int,
+    description: String,
+    bgColor: Color? = CardItemBg,
+    iconColor: Color? = IconColor,
+    boxSize: Int? = 40,
+    iconSize: Int? = 24
+) {
+    Box(
+        modifier = Modifier
+            .size(boxSize!!.dp)
+            .clip(RoundedCornerShape(10.dp))
+            .background(bgColor!!),
+        contentAlignment = Alignment.Center
+    )
+    {
+        Icon(
+            painter = painterResource(id = resId),
+            contentDescription = description,
+            modifier = Modifier.size(iconSize!!.dp),
+            tint = iconColor!!
+        )
     }
 }
 
@@ -132,6 +437,7 @@ fun OrderNowBox() {
         modifier = Modifier
             .fillMaxWidth()
             .height(156.dp)
+            .padding(end = 13.dp)
             .clip(RoundedCornerShape(20.dp))
             .background(
                 Yellow200
@@ -197,72 +503,18 @@ fun OrderNowBox() {
 }
 
 @Composable
-fun Header() {
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.SpaceBetween,
-        modifier = Modifier.fillMaxWidth()
-    ) {
-        BoxWithRes(resId = R.drawable.menu, description = "Menu")
-
-        Row(verticalAlignment = Alignment.CenterVertically)
-        {
-            Icon(
-                painter = painterResource(id = R.drawable.location),
-                contentDescription = "Location",
-                modifier = Modifier.size(16.dp),
-                tint = Orange500
-            )
-            Spacer(modifier = Modifier.width(8.dp))
-            Text(text = "California, US")
-            Spacer(modifier = Modifier.width(8.dp))
-            Icon(
-                painter = painterResource(id = R.drawable.arrow_down),
-                contentDescription = "Arrow down",
-                modifier = Modifier.size(16.dp),
-                tint = Orange500
-            )
-        }
-
-        BoxWithRes(resId = R.drawable.search, description = "Search")
-
-    }
-}
-
-@Composable
-fun BoxWithRes(
-    resId: Int,
-    description: String,
-    bgColor: Color? = CardItemBg,
-    iconColor: Color? = IconColor,
-    boxSize: Int? = 40,
-    iconSize: Int? = 24
-) {
-    Box(
-        modifier = Modifier
-            .size(boxSize!!.dp)
-            .clip(RoundedCornerShape(10.dp))
-            .background(bgColor!!),
-        contentAlignment = Alignment.Center
-    )
-    {
-        Icon(
-            painter = painterResource(id = resId),
-            contentDescription = description,
-            modifier = Modifier.size(iconSize!!.dp),
-            tint = iconColor!!
-        )
-    }
-}
-
-@Composable
 fun CategoryList(categories: List<CategoryData>) {
 
     val selectedIndex = remember {
         mutableStateOf(0)
     }
 
-    LazyRow(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
+    LazyRow(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(end = 13.dp),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
         items(categories.size) { index ->
             CategoryItem(
                 categoryData = categories[index],
@@ -307,16 +559,16 @@ fun CategoryItem(categoryData: CategoryData, selectedIndex: MutableState<Int>, i
 }
 
 @Composable
-fun PopularList(popularList: List<PopularData>) {
+fun PopularList(popularList: List<PopularData>, navController: NavController) {
     LazyColumn(modifier = Modifier.fillMaxWidth()) {
         items(popularList.size) { index ->
-            PopularItem(popularData = popularList[index])
+            PopularItem(popularData = popularList[index], navController = navController)
         }
     }
 }
 
 @Composable
-fun PopularItem(popularData: PopularData) {
+fun PopularItem(popularData: PopularData, navController: NavController) {
     Box(
         modifier = Modifier
             .fillMaxWidth()
@@ -330,6 +582,12 @@ fun PopularItem(popularData: PopularData) {
                 .clip(
                     RoundedCornerShape(18.dp)
                 )
+                .clickable {
+                    navController.currentBackStackEntry?.arguments = Bundle().apply {
+                        putParcelable(Destinations.DetailArgs.foodData, popularData)
+                    }
+                    navController.navigate(Destinations.Detail)
+                }
                 .background(CardItemBg)
         )
 
@@ -428,7 +686,11 @@ fun PopularItem(popularData: PopularData) {
                     )
                     Spacer(modifier = Modifier.width(8.dp))
 
-                    Text(text = "${popularData.price}", style = Typography.body1, color = BlackTextColor)
+                    Text(
+                        text = "${popularData.rate}",
+                        style = Typography.body1,
+                        color = BlackTextColor
+                    )
                 }
             }
         }
@@ -449,6 +711,7 @@ fun PopularItem(popularData: PopularData) {
 @Composable
 fun DefaultPreview() {
     FoodDeliveryTheme {
-        HomeScreen()
+        val navController = rememberNavController()
+        DetailScreen(navController = navController)
     }
 }
--- app/src/main/java/com/example/fooddelivery/PopularData.kt
+++ app/src/main/java/com/example/fooddelivery/PopularData.kt
@@ -1,7 +1,10 @@
 package com.example.fooddelivery
 
+import android.os.Parcelable
 import androidx.annotation.DrawableRes
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class PopularData(
     @DrawableRes val resId: Int,
     val title: String,
@@ -11,4 +14,4 @@ data class PopularData(
     val calorie: Double,
     val scheduleTime: Double,
     val ingredients: List<Int>
-)
+): Parcelable
